d94ba4267707d061f594a8ea4b0fa6fe4b041d2a
Looyoung <18813291925@163.com>
2020-02-04 16:50:56 +0800

    渲染真实的频道

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,10 @@
 <template>
    <div class="container">
     <van-tabs v-model="activeIndex" swipeable>
-      <van-tab :title="'标签' +  item" v-for="item in 10" :key="item">
+      <van-tab :title="channel.name" v-for="channel in channels" :key="channel.id">
         <!-- 这里注意 这个div设置了滚动条 目的是 给后面做 阅读记忆 留下伏笔 -->
         <!-- 阅读记忆 => 看文章看到一半 滑到中部 去了别的页面 当你回来时 文章还在你看的位置 -->
-        <ArticleList></ArticleList>
+        <article-list></article-list>
       </van-tab>
     </van-tabs>
     <span class="bar_btn">
@@ -15,10 +15,25 @@
 
 <script>
 import ArticleList from './components/article-list'
+import { getMyChannels } from '@/api/channel'
 export default {
   name: 'home', // devtools查看组件时  可以看到 对应的name名称
   components: {
     ArticleList
+  },
+  data () {
+    return {
+      channels: []
+    }
+  },
+  methods: {
+    async getMyChannels () {
+      let data = await getMyChannels()
+      this.channels = data.channels // 更新原来的channels
+    }
+  },
+  created () {
+    this.getMyChannels()
   }
 }
 </script>

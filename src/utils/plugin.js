33e37c7fa3b953fae782386abc2036905a4bc49f
Looyoung <18813291925@163.com>
2020-02-04 15:56:53 +0800

    首页-文章列表-使用tab组件

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,5 +1,5 @@
 export default {
   install (Vue) {
-    Vue.prototype.$gnotify = (params) => Vue.prototype.$notify({ duration: 500, ...params }) // 小伎俩-封装 别人的方法
+    Vue.prototype.$gnotify = (params) => Vue.prototype.$notify({ duration: 500, color: 'yellow', ...params }) // 小伎俩-封装 别人的方法
   }
 }
